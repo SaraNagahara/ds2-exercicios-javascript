36683e62a83503e377b1ae4acbe27c22c511db66
SaraNagahara <sarasfnagahara@gmail.com>
2026-08-20 15:50:04 -0300

    feat: coleta e apresenta dados do aluno

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -1,1 +1,8 @@
+const nome = prompt("Qual seu nome?");
+let idade = prompt("Qual sua idade?");
+const curso = prompt("O que esta cursando?");
+const cidade = prompt("Qual cidade você mora?");
 
+
+console.log("Olá, meu nome é " + nome + ", tenho " + idade + " anos, moro em " + cidade + " e estou cursando " +  curso + ".");
+console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} e estou cursando ${curso}.`);
