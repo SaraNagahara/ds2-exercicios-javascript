102fdd9ca0f7ff61176c01ed7cd7c63f61d86d6a
SaraNagahara <sarasfnagahara@gmail.com>
2026-08-20 16:53:32 -0300

    feat: processa numeros com laco while

--- js/exercicio09.js
+++ js/exercicio09.js
@@ -1,1 +1,41 @@
+let numero;
+let quantidade = 0;
+let soma = 0;
+let maior;
+let menor;
 
+while (true) {
+    numero = Number(prompt("Digite um número (0 para sair):"));
+
+    if (numero === 0) {
+        break;
+    }
+
+    quantidade++;
+    soma += numero;
+
+    if (quantidade === 1) {
+        maior = numero;
+        menor = numero;
+    } else {
+        if (numero > maior) {
+            maior = numero;
+        }
+
+        if (numero < menor) {
+            menor = numero;
+        }
+    }
+}
+
+if (quantidade === 0) {
+    console.log("Nenhum número foi informado.");
+} else {
+    let media = soma / quantidade;
+
+    console.log("Quantidade: " + quantidade);
+    console.log("Soma: " + soma);
+    console.log("Média: " + media);
+    console.log("Maior número: " + maior);
+    console.log("Menor número: " + menor);
+}
