53a42c41693d56e7668e7f2d54bf264804ba80c2
SaraNagahara <sarasfnagahara@gmail.com>
2026-08-20 17:02:08 -0300

    refactor: organiza calculos em guncoes reutilizaveis

--- js/exercicio11.js
+++ js/exercicio11.js
@@ -1,1 +1,36 @@
+function somar(numero1, numero2) {
+    return numero1 + numero2;
+}
 
+function calcularMedia(nota1, nota2) {
+    return (nota1 + nota2) / 2;
+}
+
+function classificarMedia(media) {
+    if (media >= 7) {
+        return "Aprovado";
+    } else if (media >= 5) {
+        return "Recuperação";
+    } else {
+        return "Reprovado";
+    }
+}
+
+function criarSaudacao(nome) {
+    return "Olá, " + nome + "! Seja bem-vindo(a).";
+}
+
+
+let resultadoSoma = somar(10, 5);
+
+let resultadoMedia = calcularMedia(8, 6);
+
+let resultadoClassificacao = classificarMedia(resultadoMedia);
+
+let resultadoSaudacao = criarSaudacao("Sara");
+
+
+console.log("Soma:", resultadoSoma);
+console.log("Média:", resultadoMedia);
+console.log("Situação:", resultadoClassificacao);
+console.log(resultadoSaudacao);
