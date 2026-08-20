53482d83fdfb796e692465fa0fc72d30e993833f
SaraNagahara <sarasfnagahara@gmail.com>
2026-08-20 16:48:04 -0300

    feat: gera tabuada utilizando for

--- js/exercicio08.js
+++ js/exercicio08.js
@@ -1,1 +1,14 @@
+//Tabuada com for
 
+let numero = parseInt(prompt("Digite um número para ver sua tabuada:"));
+
+if (!Number.isInteger(numero)) {
+    console.log("Número inválido");
+} else {    
+    console.log(`Tabuada do ${numero}:`);
+    for (let i = 1; i <= 10; i++) {
+        let resultado = numero * i;
+        console.log(`${numero} x ${i} = ${resultado}`);
+    }
+
+}
