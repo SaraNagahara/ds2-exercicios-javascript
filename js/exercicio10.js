61c42a248fd333ac546eaadf539c30e40850380c
SaraNagahara <sarasfnagahara@gmail.com>
2026-08-20 16:57:53 -0300

    feat: limita tentativas de login

--- js/exercicio10.js
+++ js/exercicio10.js
@@ -1,1 +1,23 @@
 
+const admin = "admin";
+const senha = "1234";
+
+let tentativas = 0;
+
+while (tentativas < 3) {
+    const usuario = prompt("Digite o usuário:");
+    const senhaDigitada = prompt("Digite a senha:");
+
+    if (usuario === admin && senhaDigitada === senha) {
+        console.log("Acesso permitido");
+        break;
+    } else {
+        tentativas++;
+
+        if (tentativas < 3) {
+            console.log("Login incorreto. Tentativas restantes: " + (3 - tentativas));
+        } else {
+            console.log("Acesso bloqueado");
+        }
+    }
+}
